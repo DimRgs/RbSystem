ca742a6520e22135fe277d8094bc2c19c597e3e3
Dim R <1209924748@qq.com>
2019-11-11 15:50:52 +0800

    session

--- src/system/control/UserHandler.java
+++ src/system/control/UserHandler.java
@@ -3,6 +3,8 @@ package system.control;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,23 +18,44 @@ public class UserHandler {
 	
 	@Autowired
 	private UserService us;
+	@Autowired
+	private HttpServletRequest request;
+	
+	@RequestMapping("/remove.do")
+	@ResponseBody
+	public String removeSession() throws Exception
+	{
+		request.getSession().removeAttribute("user");
+		return "remove";
+	}
 	
 	@RequestMapping("/test.do")
 	@ResponseBody
-	public Map<String, Object> JsonTest(String name, String password) throws Exception
+	public Map<String, Object> jsonTest(String name, String password) throws Exception
 	{
 		Map<String, Object> map = new HashMap<String, Object>(3);
 		map.put("test1", us.testService(1));
 		map.put("test2", us.testService(2));
 		map.put("test3", us.testService(3));
 		map.put("name", name);
-		map.put("count", us.getDepartmentCount());
+		
+		if(request.getSession().getAttribute("count") == null)
+		{
+			map.put("count", 0);
+			request.getSession().setAttribute("count", us.getDepartmentCount());
+		}
+		else 
+		{
+			map.put("count", request.getSession().getAttribute("count"));
+			
+		}
+		
 		return map;
 	}
 	
 	@RequestMapping("login.do")
 	@ResponseBody
-	public Map<String, Object> Login(String id, String password) throws Exception 
+	public Map<String, Object> login(String id, String password) throws Exception 
 	{
 		Map<String, Object> map = new HashMap<String, Object>(3);
 		UserInfo userInfo = us.getUserInfo(id);
